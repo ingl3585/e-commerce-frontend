d13288d535ae273b51c9032021502e02c11d937a
Tony Ingle <ingleanthonyco@gmail.com>
2022-05-09 17:35:21 -0600

    fetching data from backend

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from 'axios';
 
-const ProductPage = () => {
+const ProductPage = ({ match }) => {
+	const [product, setProduct] = useState([]);
 	const { id } = useParams();
-	const product = products.find((item) => item.id === Number(id));
+	useEffect(() => {
+		const fetchProduct = async () => {
+			const { data } = await axios.get(`/api/products/${Number(id)}`);
+			setProduct(data);
+		};
+		fetchProduct();
+	}, []);
 	return (
 		<div>
 			<Link to='/' className='btn btn-light my-3'>
@@ -14,24 +21,24 @@ const ProductPage = () => {
 			</Link>
 			<Row>
 				<Col md={6}>
-					<Image src={product.image} alt={product.title} fluid />
+					<Image src={product?.image} alt={product?.title} fluid />
 				</Col>
 				<Col md={3}>
 					<ListGroup variant='flush'>
 						<ListGroup.Item>
-							<h3>{product.title}</h3>
+							<h3>{product?.title}</h3>
 						</ListGroup.Item>
 					</ListGroup>
 					<ListGroup variant='flush'>
 						<ListGroup.Item>
 							<Rating
-								value={product.rating.rate}
-								text={`${product.rating.count} reviews`}
+								value={product?.rating?.rate}
+								text={`${product?.rating?.count} reviews`}
 								color={'#F8E825'}
 							/>
 						</ListGroup.Item>
-						<ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-						<ListGroup.Item>Description: {product.description}</ListGroup.Item>
+						<ListGroup.Item>Price: ${product?.price}</ListGroup.Item>
+						<ListGroup.Item>Description: {product?.description}</ListGroup.Item>
 					</ListGroup>
 				</Col>
 				<Col md={3}>
@@ -41,7 +48,7 @@ const ProductPage = () => {
 								<Row>
 									<Col>Price: </Col>
 									<Col>
-										<strong>${product.price}</strong>
+										<strong>${product?.price}</strong>
 									</Col>
 								</Row>
 							</ListGroup.Item>
@@ -49,14 +56,14 @@ const ProductPage = () => {
 								<Row>
 									<Col>Status: </Col>
 									<Col>
-										{product.rating.stock > 0 ? 'In Stock' : 'Out of Stock'}
+										{product?.rating?.stock > 0 ? 'In Stock' : 'Out of Stock'}
 									</Col>
 								</Row>
 							</ListGroup.Item>
 							<ListGroup.Item>
 								<Button
 									className='btn-block'
-									disabled={product.rating.stock === 0}
+									disabled={product?.rating?.stock === 0}
 									type='button'>
 									Add to Cart
 								</Button>
