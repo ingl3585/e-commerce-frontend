5fc75a7fc9f5bb653a85a88f19c3659260675883
Tony Ingle <ingleanthonyco@gmail.com>
2022-05-09 11:00:26 -0600

    started Header component w bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
-	return <div className='App'>E-Commerce Website</div>;
+	return (
+		<div className='App'>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<p>App</p>
+				</Container>
+			</main>
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
